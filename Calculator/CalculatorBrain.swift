0680403b34f9a1492d69a220aaf11338efcf02f6
Abey Koolipurackal <abey@virginia.edu>
2017-03-06 00:03:38 -0500

    Akkdj

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,43 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Abey Koolipurackal on 3/5/17.
+//  Copyright © 2017 abeykool. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    private var accumulator: Double?
+    
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation
+    }
+    
+    private var operations: Dictionary< String, Operation> = [
+        "π" : Operation.constant(Double.pi),
+        "e" : Operation.constant(M_E),
+        "√" : Operation.unaryOperation, //sqrt,
+        "cos" : Operation.unaryOperation //cos
+    ]
+    
+    mutating func performOperation(_ symbol: String){
+        if let constant = operations[symbol]{
+            
+        }
+        
+    }
+    
+    mutating func setOperand(_ operand: Double){
+        accumulator = operand
+    }
+    
+    var result: Double? {
+        get{
+            return accumulator
+        }
+    }
+}
